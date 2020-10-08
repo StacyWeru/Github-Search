4179c8bfeafa9adb5afa7c419877142ab6e614de
StacyWeru <stacyweru@gmail.com>
2020-10-08 18:49:41 +0300

    Added and made changes

--- src/app/users/users.component.ts
+++ src/app/users/users.component.ts
@@ -1,15 +1,25 @@
 import { Component, OnInit } from '@angular/core';
+import { InfoService } from '../info-service/info.service';
+import { User } from '../user';
 
 @Component({
   selector: 'app-users',
+  providers:[InfoService],
   templateUrl: './users.component.html',
   styleUrls: ['./users.component.css']
 })
 export class UsersComponent implements OnInit {
+  user:User;
+  username:string;
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor(private InfoService:InfoService) {
+     }
+  findUser(){
+  this.InfoService.updateUser(this.username);
+  this.InfoService.UserRequest()
+  this.user=this.InfoService.User
   }
+  ngOnInit() {}
 
 }
+
