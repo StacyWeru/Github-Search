6d107a9dcda3c1fa16b572d888fe295084f227aa
StacyWeru <stacyweru@gmail.com>
2020-10-08 16:24:34 +0300

    Made more changes

---
+++ src/app/app.module.ts
@@ -0,0 +1,38 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { NotFoundComponent } from './not-found/not-found.component';
+import { HomeComponent } from './home/home.component';
+import { InfoService } from '../app/info-service/info.service';
+import { User } from '../app/user';
+import{ UsersComponent} from "../app/user/user.component";
+import { RepositoryComponent } from './repository/repository.component';
+import { JoinDateCountPipe } from './join-date-count.pipe';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    NavBarComponent,
+    NotFoundComponent,
+    HomeComponent,
+    UsersComponent,
+    RepositoryComponent,
+    JoinDateCountPipe
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule,
+    HttpClientModule,
+  ],
+  providers: [ InfoService],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
+ 
